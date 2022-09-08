e7b84f9de7394b4ea3bad103c00fbdf9aca5c4ff
emilkjuka <emilkjuka@gmail.com>
2022-09-08 17:57:45 +0200

    Initial commit of the application
    Creating the spring boot application using online tool

---
+++ src/test/kotlin/com/j11n/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.j11n.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
